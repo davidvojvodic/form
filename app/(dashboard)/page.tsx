71c7a86ae7dca136bb321efbb48b5e35350b3655
David Vojvodić <davidvojvodic1@gmail.com>
2023-10-21 13:27:12 +0200

    stats

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,108 @@
-import Image from "next/image";
+import { GetFormStats } from "@/actions/form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ViewGridIcon } from "@radix-ui/react-icons";
+import { Suspense } from "react";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="container pt-4">
+      <Suspense fallback={<StatsCards loading={true} />}>
+        <CardStatsWrapper />
+      </Suspense>
+      <Separator className="my-6" />
+      <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
+    </div>
+  );
+}
+
+async function CardStatsWrapper() {
+  const stats = await GetFormStats();
+
+  return <StatsCards loading={false} data={stats} />;
+}
+
+interface StatsCardsProps {
+  data?: Awaited<ReturnType<typeof GetFormStats>>;
+  loading: boolean;
+}
+
+function StatsCards({ data, loading }: StatsCardsProps) {
+  return (
+    <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <StatsCard
+        title="Total visits"
+        icon={<ViewGridIcon className="text-blue-600" />}
+        helperText="All time form visits"
+        value={data?.visits.toLocaleString() || ""}
+        className="shadow-md shadow-blue-600"
+        loading={loading}
+      />
+      <StatsCard
+        title="Total submissions"
+        icon={<ViewGridIcon className="text-yellow-600" />}
+        helperText="All time submissions"
+        value={data?.submissions.toLocaleString() || ""}
+        className="shadow-md shadow-yellow-600"
+        loading={loading}
+      />
+      <StatsCard
+        title="Submission rate"
+        icon={<ViewGridIcon className="text-green-600" />}
+        helperText="Visits that result in form submission"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
+        className="shadow-md shadow-green-600"
+        loading={loading}
+      />
+      <StatsCard
+        title="Bounce rate"
+        icon={<ViewGridIcon className="text-red-600" />}
+        helperText="Visits that leave without interacting"
+        value={data?.bounceRate.toLocaleString() + "%" || ""}
+        className="shadow-md shadow-red-600"
+        loading={loading}
+      />
+    </div>
+  );
+}
+
+interface StatsCardProps {
+  title: string;
+  icon: React.ReactNode;
+  helperText: string;
+  value: string;
+  className: string;
+  loading: boolean;
+}
+
+function StatsCard({
+  title,
+  icon,
+  helperText,
+  value,
+  loading,
+  className,
+}: StatsCardProps) {
+  return (
+    <Card className={className}>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle className="text-sm font-medium text-muted-foreground">
+          {title}
+        </CardTitle>
+        {icon}
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">
+          {loading && (
+            <Skeleton>
+              <span className="opacity-0">0</span>
+            </Skeleton>
+          )}
+          {!loading && value}
+        </div>
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+      </CardContent>
+    </Card>
+  );
 }
